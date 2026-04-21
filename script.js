507b6603b4d1b15afba79e16e9122e3c7a3f3063
Sakshi Ghanwat <141915486+SakshiGhanwat@users.noreply.github.com>
2026-04-21 16:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // remove leading spaces
+  s = s.trimStart();
+  
+  // find first space
+  let index = s.indexOf(' ');
+  
+  // if no space found, return whole string
+  if (index === -1) {
+    return s;
+  }
+  
+  // return substring till first space
+  return s.slice(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
